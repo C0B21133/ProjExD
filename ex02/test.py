eb075d425963f26a0c22a50776496208e151c14c
C0B21133 <c0b2113378@edu.teu.ac.jp>
2022-10-04 13:11:44 +0900

    授業資料

---
+++ ex02/test.py
@@ -0,0 +1,31 @@
+print("hello world")
+
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    # tkm.showwarning("警告", "ボタン押したらあかんというたやろ")
+    btn = event.widget
+    txt = btn["text"]
+    tkm.showinfo(txt, f"[{txt}]ボタンが押されました")
+
+root = tk.Tk()
+root.title("おためし")
+root.geometry("500x200")
+
+label = tk.Label(root,
+                text="ラベル書いてみた",
+                font=("Ricty Diminished", 20)
+                )
+label.pack()
+
+# button = tk.Button(root, text="押すな", command=button_click)
+button = tk.Button(root, text="押すな")
+button.bind("<1>", button_click)
+button.pack()
+
+entry = tk.Entry(width=30)
+entry.insert(tk.END, "fugapiyo")
+entry.pack()
+
+root.mainloop()
