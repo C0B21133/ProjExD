c8a8a16449f5d1051dfed3493085f28fa64bf91e
C0B21133 <c0b2113378@edu.teu.ac.jp>
2022-10-04 14:40:24 +0900

    練習問題3

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
-import numpy as np
 
 class _button():
     def __init__(self, num):
@@ -11,15 +10,24 @@ class _button():
                                 height=2)
 
 
+def button_click(event):
+    btn = event.widget
+    txt = btn["text"]
+    tkm.showinfo(txt, f"{txt}のボタンが押されました")
+
 # 初期化
 root = tk.Tk()
 root.title("calc")
 root.geometry("300x500")
 
-id = [[j, k] for j in range(4) for k in range(3)]
-buttons = [_button(n) for n in range(9, -1, -1)]
+# ボタン作成
+index = [[j, k] for j in range(4) for k in range(3)]
+index = index[:10]
+index.reverse()
+buttons = [_button(n) for n in range(10)]
 
 for i in range(10):
-    buttons[i].button.grid(row=id[i][0], column=id[i][1])
+    buttons[i].button.grid(row=index[i][0], column=index[i][1])
+    buttons[i].button.bind("<1>", button_click)
 
 root.mainloop()
