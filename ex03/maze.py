1c0ab2882d18833622b3a8f4d7b3e4ea081f0bce
C0B21133 <c0b2113378@edu.teu.ac.jp>
2022-10-18 13:07:04 +0900

    練習問題1、２

---
+++ ex03/maze.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+if __name__ == "__main__":
+    # 初期化
+    root = tk.Tk()
+    root.title("calc")
+    root.geometry("1500x900")
+    root.configure(bg='black')
+
+    root.mainloop()
