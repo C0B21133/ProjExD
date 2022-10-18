e5c11c21a92e3e78c2a6b70d20944f144794d9a8
C0B21133 <c0b2113378@edu.teu.ac.jp>
2022-10-18 13:22:21 +0900

    練習問題2修正、3

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,8 +3,12 @@ import tkinter as tk
 if __name__ == "__main__":
     # 初期化
     root = tk.Tk()
-    root.title("calc")
-    root.geometry("1500x900")
-    root.configure(bg='black')
+    root.title("maze")
 
+    canvas = tk.Canvas(root, width=1500, height=900, bg='black')
+    canvas.pack()
+
+    tori = tk.PhotoImage(file = "./ex03/fig/0.png")
+    cx, cy = 300, 400
+    canvas.create_image(cx, cy, image=tori, tag="tori")
     root.mainloop()
