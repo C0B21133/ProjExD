3222172af256068373315985e81b4a82e2a9431f
C0B21133 <c0b2113378@edu.teu.ac.jp>
2022-10-18 13:38:29 +0900

    練習問題5

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,9 +1,16 @@
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym
+
+
 if __name__ == "__main__":
     # 初期化
     root = tk.Tk()
     root.title("maze")
+    key = ""
+    root.bind("<KeyPress>", key_down)
 
     canvas = tk.Canvas(root, width=1500, height=900, bg='black')
     canvas.pack()
@@ -11,7 +18,5 @@ if __name__ == "__main__":
     tori = tk.PhotoImage(file = "./ex03/fig/0.png")
     cx, cy = 300, 400
     canvas.create_image(cx, cy, image=tori, tag="tori")
-    
-    key = ""
-    
+
     root.mainloop()
