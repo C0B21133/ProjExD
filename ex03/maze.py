a1b5a5eb98c88372145429d9df42f8f6cfdc14fb
C0B21133 <c0b2113378@edu.teu.ac.jp>
2022-10-18 14:17:51 +0900

    練習問題9

--- ex03/maze.py
+++ ex03/maze.py
@@ -36,6 +36,8 @@ if __name__ == "__main__":
     canvas = tk.Canvas(root, width=1500, height=900, bg='black')
     canvas.pack()
 
+    mazelist = mm.make_maze(15, 9)
+
     tori = tk.PhotoImage(file = "./ex03/fig/0.png")
     cx, cy = 300, 400
     canvas.create_image(cx, cy, image=tori, tag="tori")
