7314563f4b50d8eab99174296e56264e5bce4622
C0B21133 <c0b2113378@edu.teu.ac.jp>
2022-10-25 16:48:37 +0900

    爆弾複数追加

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -9,6 +9,26 @@ from random import randint
 #     tori_rct.center = x, y
 #     sfc.blit(tori_sfc, tori_rct)
 
+class bomb():
+    count = 0
+    def __init__(self, scrn_rct):
+        # カウント初期化
+        bomb.count = 2300
+        self.vx = 1
+        self.vy = 1
+        self.bomb_sfc = pg.Surface((20, 20))
+        pg.draw.circle(self.bomb_sfc, (255, 0, 0), (10, 10), 10)
+        self.bomb_sfc.set_colorkey((0, 0, 0))
+        self.bomb_rct = self.bomb_sfc.get_rect()
+        self.bomb_rct.centerx = randint(0, scrn_rct.width)
+        self.bomb_rct.centery = randint(0, scrn_rct.height) 
+    def move(self, scrn_sfc, scrn_rct):      
+        yoko, tate = check_bound(self.bomb_rct, scrn_rct)
+        self.vx *= yoko
+        self.vy *= tate        
+        self.bomb_rct.move_ip(self.vx, self.vy)
+        scrn_sfc.blit(self.bomb_sfc, self.bomb_rct)
+
 def check_bound(obj_rct, scr_rct):
     """
     obj_rct: こうかとんor爆弾, scr_rct: スクリーン
@@ -22,10 +42,20 @@ def check_bound(obj_rct, scr_rct):
         tate = -1
     return yoko, tate
 
+def gameover(scrn_sfc):
+    fonts = pg.font.Font(None, 80)
+    txt = fonts.render(str("GAMEOVER"), True, (255, 0, 0))
+    scrn_sfc.blit(txt, (620, 350))
+    pg.display.update()
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: 
+                return True 
+
 
 def main():
     pg.display.set_caption("pygame")
-    scrn_sfc = pg.display.set_mode((1000, 600))
+    scrn_sfc = pg.display.set_mode((1500, 800))
     scrn_rct = scrn_sfc.get_rect()
     # 背景
     back_sfc = pg.image.load("fig/pg_bg.jpg")
@@ -36,20 +66,23 @@ def main():
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
     # 爆弾
-    vx = 1; vy = 1
-    bomb_sfc = pg.Surface((20, 20))
-    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
-    bomb_sfc.set_colorkey((0, 0, 0))
-    bomb_rct = tori_sfc.get_rect()
-    bomb_rct.centerx = randint(0, scrn_rct.width)
-    bomb_rct.centery = randint(0, scrn_rct.height)
-    # クロック
+    # vx = 1; vy = 1
+    # bomb_sfc = pg.Surface((20, 20))
+    # pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    # bomb_sfc.set_colorkey((0, 0, 0))
+    # bomb_rct = bomb_sfc.get_rect()
+    # bomb_rct.centerx = randint(0, scrn_rct.width)
+    # bomb_rct.centery = randint(0, scrn_rct.height)
+    bombs = []
+    bombs.append(bomb(scrn_rct))
     clock = pg.time.Clock()
-    clock.tick(1000)    
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        bomb.count -= 1
+        if not bomb.count:
+            bombs.append(bomb(scrn_rct))
+
 
         # こうかとんキー処理
         key_lst = pg.key.get_pressed()
@@ -73,16 +106,21 @@ def main():
             if key_lst[pg.K_DOWN]:
                 tori_rct.centery -= 1  
 
-        yoko, tate = check_bound(bomb_rct, scrn_rct)
-        vx *= yoko
-        vy *= tate        
-        bomb_rct.move_ip(vx, vy)
+        # yoko, tate = check_bound(bomb_rct, scrn_rct)
+        # vx *= yoko
+        # vy *= tate        
+        # bomb_rct.move_ip(vx, vy)
 
-        if tori_rct.colliderect(bomb_rct): return
-        
         scrn_sfc.blit(back_sfc, back_rct)
         scrn_sfc.blit(tori_sfc, tori_rct)
-        scrn_sfc.blit(bomb_sfc, bomb_rct)
+        # scrn_sfc.blit(bomb_sfc, bomb_rct)
+
+        for b in bombs:
+            b.move(scrn_sfc, scrn_rct)
+            if tori_rct.colliderect(b.bomb_rct): 
+                if gameover(scrn_sfc): return        
+        # クロック 
+        clock.tick(1000)  
         pg.display.update()
 
 if __name__ == "__main__":
